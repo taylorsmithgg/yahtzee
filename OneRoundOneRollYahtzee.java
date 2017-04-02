8ce1dc8a782ac76e9b94dbc3d8f608984f44e731
Taylor Smith <taylor.smithgg@gmail.com>
2017-04-02 15:42:59 -0500

    Update OneRoundOneRollYahtzee.java

--- OneRoundOneRollYahtzee.java
+++ OneRoundOneRollYahtzee.java
@@ -1,110 +1,164 @@
-
-/**
- * Write a description of class OneRoundOneRollYahtzee here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import java.util.Arrays;
 import java.util.Scanner;
+
 public class OneRoundOneRollYahtzee
 {
+    private static Scanner input = new Scanner(System.in);
+
+    private static int rules;
+    private static int[] rolls = new int[5];
+
+
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
+        getChoice();
+    }
+
+    private static void getChoice()
+    {
         System.out.print("Please enter 1) to roll your own dice, 2) to let the computer roll the dice, or 3) to quit: ");
         int choice = input.nextInt();
-        
-        if(choice == 1)
-        {
-           decision1();
-           //categories();
-        }
-        else if(choice == 2)
-        {
-           decision2();
-           //categories();
-        }
-        else
-        {
-            System.out.println("Have a nice day!");
-        }    
-    }
-    public static void decision1()
-    {
-        Scanner input = new Scanner(System.in);
-       //int[] list;
-       
-       System.out.print("Please enter the five dice rolls (seperated by spaces): ");
-       String myList = input.nextLine();
-       char[] list = new char[myList.length()];
-       
-       String order = "";
-       for (int i = 0; i < list.length; i++) {
-            list[i] = myList.charAt(i);
-            
-        }
-       char temp = '0';
-       for (int i = 0; i < list.length; i++){
-            for (int j = i + 1; j < list.length; j++) {
-                if (list[i] > list[j]) {
-                    temp = list[i];
-                    list[i] = list[j];
-                    list[j] = temp;
-                    
-                }
-            }
-        }
-       System.out.print("Here are the digits in non-inceasing order: ");
-       for (int i = 0; i < list.length; i++) 
-       {
-           if(list[i] != ' ')
-            order += (list[i] + " ");
-           //System.out.print(list[i] + ",");
-       }
 
-       System.out.print(order);
-    }
-    public static void decision2()
-    {
- 
-      int number = 5;
-      int[] random = new int[number];
-      int count = 1;
-      String increasing = "";
-       
-       for(int i = 0; i < number; i++)
-        {
-           random[i] = (int)(6.0 * Math.random() + 1.0);
+        switch(choice){
+            case 1:
+                decision1();
+                break;
+            //categories();
+            case 2:
+                for(int i = 0; i < 5; i++){
+                    rolls[i] = decision2();
+                }
+                System.out.print("The 5 rolls in non-decreasing order are: " + Arrays.toString(rolls));
+                break;
+            case 3:
+                return;
+            default:
+                System.out.println("Invalid choice. Please select 1 or 2");
+                getChoice();
         }
-       for(int i = 0; i < number; i++){
-          for(int j = i + 1; j < number; j++){
-              if(random[i] > random[j]){
-                   int temp = random[i];
-                   random[i] = random[j];
-                   random[j] = temp;}
+
+        categories();
+        getChoice();
+    }
+
+    private static void decision1()
+    {
+        System.out.print("Please enter the five dice rolls (separated by spaces): ");
+        String myList = input.nextLine();
+        char[] list = myList.trim().replaceAll(" ", "").toCharArray();
+
+        for(char c : list){
+            if(Character.getNumericValue(c) > 6 || Character.getNumericValue(c) < 1){
+                System.out.println("You entered an invalid dice roll: " + c);
+                System.out.println("Please try again.");
+                decision1();
+                return;
             }
         }
-       for (int i = 0; i < number; i++) 
-        {
-           increasing += (random[i] + " ");
+
+        if(myList.length() < 5){
+            System.out.println("You did not enter 5 digits, please try again.");
+            decision1();
+            return;
         }
-         System.out.print("The 5 rolls in non-decreasing order are: " + increasing);
-       }
-      public static void categories()
-      {
-        Scanner input = new Scanner(System.in);
-        String[] selection = {"Not Valid", "1. Aces", "2. Twos", "3. Threes", "4. Fours", "5. Fives", "6. Sixes", "7. Three of a kind", "8. Four of a kind", "9. Full House", "10. Small straight", "11. Large Straight", "12. Yahtzee", "13. Chance"};
-        //String[] Lower = {"7. Three of a kind", "8. Four of a kind", "9. Full House", "10. Small straight", "11. Large Straight", "12. Yahtzee", "13. Chance"};
+
+        String order = "";
+
+        System.out.print("Here are the digits in non-increasing order: ");
+        for (int i : list)
+        {
+            order = order.concat(String.valueOf(Character.getNumericValue(i))).concat(" ");
+        }
+
+        System.out.print(order);
+    }
+
+    private static int decision2()
+    {
+        return (int)(6.0 * Math.random() + 1.0);
+    }
+
+    public static void categories()
+    {
+        String[] upperSelection = {
+                "Aces",
+                "Twos",
+                "Threes",
+                "Fours",
+                "Fives",
+                "Sixes"
+        };
+
+        String[] lowerSelection = {
+                "Three of a kind",
+                "Four of a kind",
+                "Full House",
+                "Small straight",
+                "Large Straight",
+                "Yahtzee",
+                "Chance"
+        };
+
+        String[] selection = concat(upperSelection, lowerSelection);
         System.out.println("\nYou may choose to score from the following categories");
         System.out.println("Upper selection");
-        for(int i = 1; i <= 6; i++){
-           //Upper[i+1] = Upper[0];
-           System.out.println(selection[i]);
+        int i = 1;
+        for(String s : upperSelection){
+            System.out.println(i + ". " + s);
+            i++;
         }
-        System.out.println();
         System.out.println("Lower Selection");
-        for(int i = 7; i <= 13; i++){
-            System.out.println(selection[i]);}
+        for(String s : lowerSelection) {
+            System.out.println(i + ". " + s);
+            i++;
         }
-   
+
+        System.out.println("Please enter the number corresponding to your chosen category:");
+        rules = input.nextInt();
+        System.out.println("Your score for the " + selection[rules - 1] + " category is: " + eval());
+    }
+
+    private static int eval(){
+        switch(rules) {
+            case 1:
+                return 0;
+            case 2:
+                return 0;
+            case 3:
+                return 0;
+            case 4:
+                return 0;
+            case 5:
+                return 0;
+            case 6:
+                return 0;
+            case 7:
+                return 0;
+            case 8:
+                return 0;
+            case 9:
+                return 0;
+            case 10:
+                return 0;
+            case 11:
+                return 0;
+            case 12:
+                return 0;
+            case 13:
+                return 0;
+            default:
+                System.out.println("The Yahtzee category is not legal for this roll: " + Arrays.toString(rolls));
+                return eval();
+        }
+    }
+
+    private static String[] concat(String[] a, String[] b) {
+        int aLen = a.length;
+        int bLen = b.length;
+        String[] c= new String[aLen+bLen];
+        System.arraycopy(a, 0, c, 0, aLen);
+        System.arraycopy(b, 0, c, aLen, bLen);
+        return c;
+    }
+
 }
